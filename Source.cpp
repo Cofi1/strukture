81c1eb0325f68ccb59866201ac40548fea007f16
Cofi1 <jbozur00@fesb.hr>
2024-10-07 19:47:42 +0200

    jeje

--- Source.cpp
+++ Source.cpp
@@ -1,5 +1,27 @@
-#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#define MAX 1024
+
+int BrojRedova(const char*);
+
 int main() {
-	printf("Hello Wofgwegrld");
+	int brojredova = 0;
+	brojredova = BrojRedova("filename.txt");
 	return 0;
+}
+
+int BrojRedova(const char*) {
+	FILE* fp = NULL;
+	int brojredova = 0;
+	char buffer[MAX] = {};
+	fp = fopen(filename, "r");
+	if (!fp) {
+		return -1;
+	}
+	while (!feof(fp)) {
+		fgets(buffer, MAX, fp);
+		brojredova++;
+	}
+	fclose(fp);
+	return brojredova;
 }
